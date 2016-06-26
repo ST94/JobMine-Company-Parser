0255c18c550bc7900bd75af376325c8a9247e02f
ST94 <ST94.dev@gmail.com>
2016-06-26 13:09:32 -0400

    completed parser

--- JobMineParser.py
+++ JobMineParser.py
@@ -1,26 +1,38 @@
 from html.parser import HTMLParser
 
+
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
+    recording = False
+    companyNameArray = []
+
     def handle_starttag(self, tag, attrs):
         #print ("Encountered a start tag:", tag)
-        if tag == 'td':
-            print ('td')
+        if tag == 'div':
+            #print ('td')
             for attr,attr_value in attrs:
-                print (attr, attr_value)
-                if attr == 'col':
-                    print ('col')
-                    if attr_value =='2':
-                        print (self)
+                #print (attr, attr_value)
+                if 'UW_CO_EMPLYR_NAME' in attr_value:
+                    self.recording = True
+                if 'UW_CO_PARENT_NAME' in attr_value:
+                    self.recording = True
 
     #def handle_endtag(self, tag):
         #print ("Encountered an end tag :", tag)
 
-    #def handle_data(self, data):
-        #print ("Encountered some data  :", data)
+    def handle_data(self, data):
+        if self.recording:
+            #print (data)
+            self.companyNameArray.append(data)
+            self.recording = False
+
+
 
 # instantiate the parser and fed it some HTML
 
 with open('data/data.html', 'r') as my_file:
     parser = MyHTMLParser()
     parser.feed(my_file.read())
+    print (parser.companyNameArray)
+
+
